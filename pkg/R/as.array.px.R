ad53c18417fbdf58153b570783204ada1bcd1bd3
viciana <viciana@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-07 17:09:57 +0000

    Fixed problems with array names in as.array.px
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.array.px.R
+++ pkg/R/as.array.px.R
@@ -11,14 +11,26 @@
 #################################################################
 
 as.array.px <- function(x,... ){
-  names.vals <- c( x$HEADING$value, rev( x$STUB$value ) )
-  ## Reordena los valores para que cuadren con los datos
-  ord <- match(names.vals, names(x$VALUES)) 
-  ## Crea array
-  result <- array( x$DATA$value, 
-                  unlist(lapply(x$VALUES[ord],length)),
-                  dimnames=x$VALUES[ord] )
-##  names( result ) <- names.vals
-
-  result
+  
+    names.vals      <- c( x$HEADING$value, rev( x$STUB$value ) ) ; str(names.vals)
+    
+    #  create array
+    result <- array( x$DATA[[1]], unlist(lapply(x$VALUES[names.vals],length)), dimnames=x$VALUES[names.vals] )
+    names( dimnames(result) ) <- names.vals
+    
+    return(result)
+  
 }
+
+
+# test against some INE's PC-Axis files
+# province.numbers <- sapply( 1:5, function( x ) sprintf( "%05d", x ) )
+# province.numbers <- paste( "http://www.ine.es/pcaxisdl//t20/e245/p05/a2010/l0/", province.numbers, "001.px", sep = "" )
+
+#  debug( read.px )
+# res <- read.px( url( province.numbers[1] ) )
+# res.dat <- as.array.px( res )  # compare: 
+                                 # http://www.ine.es/jaxi/tabla.do?path=/t20/e245/p05/a2010/l0/&file=00001001.px&type=pcaxis&L=0
+# str(res.dat)
+
+# res <- sapply( province.numbers, function( x ) as.array.px( read.px( url( x ) ) ), simplify = F )
