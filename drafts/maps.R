8dca13ebe750d7b9774d703d7ee30192a984b427
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-24 23:45:59 +0000

    shapefiles and data to use with maps.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- drafts/maps.R
+++ drafts/maps.R
@@ -8,7 +8,7 @@ library(maptools)
 ## Cifras oficiales de población resultantes de la revisión del Padrón municipal a 1 de enero de 2009
 ## http://www.ine.es/jaxi/menu.do?type=pcaxis&path=%2Ft20%2Fe260%2Fa2009%2F&file=pcaxis&N=&L=0
 
-datPX <- read.px('pcaxis-676270323.px')
+datPX <- read.px('data/pcaxis-676270323.px')
 dat <- as.data.frame(datPX)
 ##Me quedo sólo con Total (dejo fuera distinción por sexo)
 datTotal <- dat[dat$sexo == 'Total',- 1]
@@ -22,7 +22,7 @@ datTotal <- datTotal[-1,]
 ## ShapeFiles de
 ## http://www.ine.es/ss/Satellite?blobcol=urldata&blobheader=application%2Fzip&blobheadername1=Content-Disposition&blobheadervalue1=attachment%3B+filename%3Dmapas_completo_municipal.zip&blobkey=urldata&blobtable=MungoBlobs&blobwhere=250%2F237%2Fmapas_completo_municipal%2C2.zip&ssbinary=true
 
-mapa <- readShapePoly('spain_provinces_img_ind_2.shp')
+mapa <- readShapePoly('shp/spain_provinces_img_ind_2.shp')
 mapa$NOMBRE99
 mapa$PROV
 dim(mapa)
@@ -42,7 +42,7 @@ spplot(mapa['dat'], col.regions=cols, at=int$brks)
 ####################################################################################
 
 ## Ejemplo de Emilio
-ficheropx <- read.px(filename="pcaxis824627184.px")
+ficheropx <- read.px(filename="data/pcaxis824627184.px")
 summary(ficheropx)
 nombremapa <- ficheropx$MAP
 nombremapa
