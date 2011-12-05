5413da9ca04081b5358d0f1f3ce2995b886e159a
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-05 12:49:06 +0000

    Emilio as new coauthor; fix in as.data.frame (CODES); small fix in help page of write.px
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.data.frame.px.R
+++ pkg/R/as.data.frame.px.R
@@ -11,22 +11,28 @@
 #################################################################
 
 as.data.frame.px <- function( x, row.names, optional, use.codes = FALSE, ... ){
-    names.vals      <- c( rev(x$HEADING$value), rev( x$STUB$value ) )
+  names.vals      <- c( rev(x$HEADING$value), rev( x$STUB$value ) )
 
-    values <- x$VALUES
+  values <- x$VALUES
 
-    if( is.logical( use.codes ) && use.codes )
-        use.codes <- names( x$CODES )
+  if( is.logical( use.codes ) && use.codes )
+    use.codes <- names( x$CODES )
 
-    if( ! is.logical( use.codes ) ){
-        for( var.name in intersect( use.codes, names( x$CODES ) ) ){
-            values[[var.name]] <- x$CODES[[ var.name ]] 
-        }
+  if( ! is.logical( use.codes ) ){
+    for( var.name in intersect( use.codes, names( x$CODES ) ) ){
+      if( var.name %in% names.vals ) {
+        var.name.value <- var.name
+      } else {
+        var.name.value <- agrep(var.name, names.vals, value="TRUE")
+      }
+      values[[var.name.value]] <- x$CODES[[var.name]]
+      ##            values[[var.name]] <- x$CODES[[ var.name ]] 
     }
+  }
 
-    result          <- data.frame( do.call(expand.grid, values[ names.vals ] ), x$DATA$value )
-    names( result ) <- c( names.vals, "dat" )
+  result          <- data.frame( do.call(expand.grid, values[ names.vals ] ), x$DATA$value )
+  names( result ) <- c( names.vals, "dat" )
 
-    result
+  result
 }
 
