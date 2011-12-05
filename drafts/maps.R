e90280af843d1751db987b72debc1e6c2e6d41dd
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-05 13:59:40 +0000

    fix code in examples to draw maps with read.pxini
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- drafts/maps.R
+++ drafts/maps.R
@@ -1,6 +1,10 @@
 library(pxR)
 library(maptools)
 
+unquote <- function(x){
+  gsub('\\"', "", x)
+}
+
 ##Ejemplo de Oscar
 
 ## setwd('~/Investigacion/pxR/drafts')
@@ -9,27 +13,24 @@ library(maptools)
 ## http://www.ine.es/jaxi/menu.do?type=pcaxis&path=%2Ft20%2Fe260%2Fa2009%2F&file=pcaxis&N=&L=0
 
 datPX <- read.px('data/pcaxis-676270323.px')
-dat <- as.data.frame(datPX)
+dat <- as.data.frame(datPX,  use.codes = TRUE)
+
 ##Me quedo sólo con Total (dejo fuera distinción por sexo)
 datTotal <- dat[dat$sexo == 'Total',- 1]
-##Extraigo el ID de cada provincia, necesario para relacionarlo con el Shapefile
-provID <- strsplit(as.character(datTotal$provincias), split = ' ')
-provID <- sapply(provID,  function(x)x[1])
-datTotal$ID <- provID
 ##elimino fila de Total
 datTotal <- datTotal[-1,]
 
 ## ShapeFiles de
 ## http://www.ine.es/ss/Satellite?blobcol=urldata&blobheader=application%2Fzip&blobheadername1=Content-Disposition&blobheadervalue1=attachment%3B+filename%3Dmapas_completo_municipal.zip&blobkey=urldata&blobtable=MungoBlobs&blobwhere=250%2F237%2Fmapas_completo_municipal%2C2.zip&ssbinary=true
-
-mapa <- readShapePoly('shp/spain_provinces_img_ind_2.shp')
+nombremapa <- unquote(datPX$MAP[1]) ##ups, lo ponen dos veces,  de ahí el [1]
+mapas <- read.pxini(nombremapa)$filesshp
+mapa <- readShapePoly(mapas)
 mapa$NOMBRE99
 mapa$PROV
 dim(mapa)
 ## Relaciono el data.frame con el shapefile a través de sus IDs
 ## Debieramos probar a hacer esto con spCbind
-idx <- match(mapa$PROV,  datTotal$ID)
-
+idx <- match(mapa$PROV,  datTotal$provincias)
 mapa$dat <- datTotal$dat[idx]
 
 ##Elaboro mapa. Elijo intervalos con classIntervals
@@ -44,41 +45,20 @@ spplot(mapa['dat'], col.regions=cols, at=int$brks)
 ## Ejemplo de Emilio
 ficheropx <- read.px(filename="data/pcaxis824627184.px")
 summary(ficheropx)
-nombremapa <- ficheropx$MAP
-nombremapa
-nombremapasincomillas <- gsub("\"","", nombremapa) ## unquote de read.px
-nombremapasincomillas ## humm, es un .ini
+dat82 <- as.data.frame(ficheropx,  use.codes = TRUE)
+nombremapa <- unquote(ficheropx$MAP)
+mapasINFO <- read.pxini(nombremapa)
 
-## http://www.ine.es/ss/Satellite?L=0&c=Page&cid=1254735116596&p=1254735116596&pagename=ProductosYServicios%2FPYSLayout
-## hay que que ver los mapas que hay en el .ini
-## [MAPS]
-## NumMaps=2
-## 1=C:\Archivos de programa\PX-Map\Maps\spain_provinces_img_ind_4.shp
-## 2=C:\Archivos de programa\PX-Map\Maps\spain_regions_img_ind.shp
-filename <- paste(nombremapasincomillas,".ini", sep="")
-encoding = "latin1"
-a <- scan(filename, what = "character", sep = "\n", quiet = TRUE, 
-        fileEncoding = encoding)
-## ¿qué lineas tienen un .shp?
-lineasconshp <- a[grepl(".shp",a, ignore.case=TRUE)]
-lineasconshp
-## ¿Cómo sacamos los ficheros shp?
-separamos <- unlist(strsplit(lineasconshp,"Maps\\",fixed=TRUE))
-ficherosshp<- separamos[grepl(".shp",separamos, ignore.case=TRUE)]
-ficherossinextension <- sub("^([^.]*).*", "\\1", ficherosshp)
+
 ## Dibujamos los mapas
-library(maptools)
-ficherossinextension[1]
-ficherossinextension[2]
-mapa1 <- readShapePoly(ficherossinextension[1])
-mapa2 <- readShapePoly(ficherossinextension[2])
 
-## Error en read.dbf(filen) : unable to open DBF file
-# en readShapePoly llama a read.shape
-## read.shape(file = ficherossinextension[1])
-## Error en read.dbf(filen) : unable to open DBF file
-## read.dbf(ficherossinextension[1])
-# Error en read.dbf(ficherossinextension[1]) : unable to open DBF file
-## read.dbf(paste(ficherossinextension[1],".dbf",sep="")) # OK, hay que añadirle la extensión
+mapaPROV <- readShapePoly(mapasINFO$filesshp[1])
+idx <- match(mapaPROV$PROV,  dat82[, 2])
+mapaPROV$dat <- dat82$dat[idx]
 
-  
+int <- classIntervals(mapaPROV$dat, 10, style='jenks')
+cols <- heat_hcl(10)
+spplot(mapaPROV['dat'], col.regions=cols, at=int$brks)
+
+
+mapaCOM <- readShapePoly(mapasINFO$filesshp[2])
