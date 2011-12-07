246125bad3662c451b2d2c50cab9cbab97f5bc8e
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-07 22:36:25 +0000

    clean maps.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- drafts/maps.R
+++ drafts/maps.R
@@ -6,44 +6,12 @@ library(colorspace)
 source('read.ini.R')
 source('../pkg/R/as.data.frame.px.R')##nueva versión no incluida en 0.24
 
+## setwd('~/Investigacion/pxR/drafts')
+
 unquote <- function(x){
   gsub('\\"', "", x)
 }
 
-##Ejemplo de Oscar
-
-## setwd('~/Investigacion/pxR/drafts')
-
-## Cifras oficiales de población resultantes de la revisión del Padrón municipal a 1 de enero de 2009
-## http://www.ine.es/jaxi/menu.do?type=pcaxis&path=%2Ft20%2Fe260%2Fa2009%2F&file=pcaxis&N=&L=0
-
-datPX <- read.px('data/pcaxis-676270323.px')
-dat <- as.data.frame(datPX,  use.codes = TRUE)
-
-##Me quedo sólo con Total (dejo fuera distinción por sexo)
-datTotal <- dat[dat$sexo == 'Total',- 1]
-##elimino fila de Total
-datTotal <- datTotal[-1,]
-
-## ShapeFiles de
-## http://www.ine.es/ss/Satellite?blobcol=urldata&blobheader=application%2Fzip&blobheadername1=Content-Disposition&blobheadervalue1=attachment%3B+filename%3Dmapas_completo_municipal.zip&blobkey=urldata&blobtable=MungoBlobs&blobwhere=250%2F237%2Fmapas_completo_municipal%2C2.zip&ssbinary=true
-nombremapa <- unquote(datPX$MAP[1]) ##ups, lo ponen dos veces,  de ahí el [1]
-mapas <- read.pxini(nombremapa)$filesshp
-mapa <- readShapePoly(mapas)
-mapa$NOMBRE99
-mapa$PROV
-dim(mapa)
-## Relaciono el data.frame con el shapefile a través de sus IDs
-## Debieramos probar a hacer esto con spCbind
-idx <- match(mapa$PROV,  datTotal$provincias)
-mapa$dat <- datTotal$dat[idx]
-
-##Elaboro mapa. Elijo intervalos con classIntervals
-int <- classIntervals(mapa$dat, 10, style='jenks')
-cols <- heat_hcl(10)
-spplot(mapa['dat'], col.regions=cols, at=int$brks)
-
-####################################################################################
 
 plotPX <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style='fisher', ...){
 
@@ -70,9 +38,6 @@ plotPX <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style
     mapi <-  readShapePoly(mapsInfo$filesshp[i])
     field <- mapsInfo$keyfields[i]
     ## Relaciono el data.frame con el shapefile a través de sus IDs
-    ## Debieramos probar a hacer esto con spCbind
-    ## idx <- match(mapi[[field]], dat[,iCol])
-    ## mapi$dat <- dat$dat[idx]
 
     idx <- match(mapi[[field]], datWide[,iCol])
     dat2add <- data.frame(datWide[idx, select]) ##Hace falta cuando sólo es una columna
@@ -89,7 +54,10 @@ plotPX <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style
   do.call(c, mapsList)
 }
 
+
 ficheropx <- read.px(filename="data/pcaxis824627184.px")
+## Cifras oficiales de población resultantes de la revisión del Padrón municipal a 1 de enero de 2009
+## http://www.ine.es/jaxi/menu.do?type=pcaxis&path=%2Ft20%2Fe260%2Fa2009%2F&file=pcaxis&N=&L=0
 datPX <- read.px('data/pcaxis-676270323.px')
 
 summary(ficheropx)
@@ -100,6 +68,9 @@ plotPX(ficheropx)
 
 plotPX(datPX)
 plotPX(datPX, select=c('Mujeres', 'Varones'))
+
+
+
 
 ##problema con Madrid y Andalucía
 codes <- as.data.frame(ficheropx, use.codes=TRUE)[,2]
