a8826d464df51a3768396c5e77208c11c63811d4
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-09 00:55:28 +0000

    plotPX is now plot.px; uses as.data.frame with direction='wide'; classInt is not used (provisional); px files with better shapefiles
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- drafts/maps.R
+++ drafts/maps.R
@@ -1,40 +1,37 @@
 library(pxR)
+library(sp)
+library(lattice)
+library(latticeExtra)
 library(maptools)
-library(classInt)
-library(colorspace)
+#library(classInt)
+#library(colorspace)
 
 source('read.ini.R')
 source('../pkg/R/as.data.frame.px.R')##nueva versión no incluida en 0.24
 
-## setwd('~/Investigacion/pxR/drafts')
+
 
 unquote <- function(x){
   gsub('\\"', "", x)
 }
 
 
-plotPX <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style='fisher', ...){
+plot.px <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style='fisher', ...){
 
-  dat <- as.data.frame(x,  use.codes = TRUE)  
+  heading <- x$HEADING$value
+  vars <- x$VALUES[[heading]]
+
+  datWide <- as.data.frame(x,  use.codes = TRUE, direction = 'wide')  
 
   mapName <- unique(unquote(x$MAP))[1]
   mapsInfo <- read.pxini(filename = mapName, shpPath=shpPath, encoding=encoding)
   nmaps <- mapsInfo$nummaps
 
-  heading <- x$HEADING$value
-  stub <- x$STUB$value
-  vars <- levels(dat[,heading])
-  datWide <- reshape(dat, timevar=heading, ## Incorporarlo a as.data.frame.px
-                     varying=list(vars),
-                     v.names='dat',
-                     idvar=stub,
-                     direction='wide')
-
   if (missing(select)) select <- vars
 
   iCol <- agrep(unique(names(x$MAP))[1], names(datWide))
 
-  fooPlot <- function(i, mapsInfo, datWide, iCol, n, style, ...) {
+  fooPlot <- function(i, mapsInfo, datWide, iCol, ...){##n, style, ...) {
     mapi <-  readShapePoly(mapsInfo$filesshp[i])
     field <- mapsInfo$keyfields[i]
     ## Relaciono el data.frame con el shapefile a través de sus IDs
@@ -44,38 +41,33 @@ plotPX <- function(x, select, shpPath = 'shp',  encoding = "latin1", n=10, style
     names(dat2add) <- select
     mapi@data <- cbind(mapi@data, dat2add)
 
-    int <- classIntervals(c(as.matrix(dat2add)), n, style=style)
-    cols <- sequential_hcl(length(int$brks)) ##incluirlo como argumento de la función
-    p <- spplot(mapi[select], col.regions=cols, at=int$brks, ...)
+    ## int <- classIntervals(c(as.matrix(dat2add)), n, style=style)
+    ## cols <- sequential_hcl(length(int$brks)) ##incluirlo como argumento de la función
+    p <- spplot(mapi[select], ...)## col.regions=cols, at=int$brks, ...)
     p
   }
-  mapsList <- lapply(1:nmaps, fooPlot, mapsInfo, datWide, iCol, n, style, ...)
+  mapsList <- lapply(1:nmaps, fooPlot, mapsInfo, datWide, iCol)##, n, style)
   names(mapsList) <- mapsInfo$keyfields
   do.call(c, mapsList)
 }
 
-
-ficheropx <- read.px(filename="data/pcaxis824627184.px")
 ## Cifras oficiales de población resultantes de la revisión del Padrón municipal a 1 de enero de 2009
 ## http://www.ine.es/jaxi/menu.do?type=pcaxis&path=%2Ft20%2Fe260%2Fa2009%2F&file=pcaxis&N=&L=0
-datPX <- read.px('data/pcaxis-676270323.px')
+
+##ficheros alterados para que usen un mapa más detallado
+
+ficheropx <- read.px(filename="data/pcaxis824627184_2.px")
+datPX <- read.px('data/pcaxis-676270323_2.px') 
 
 summary(ficheropx)
 summary(datPX)
 
 
-plotPX(ficheropx)
+plot(ficheropx)
 
-plotPX(datPX)
-plotPX(datPX, select=c('Mujeres', 'Varones'))
-
-
+plot(datPX)
+plot(datPX, select=c('Mujeres', 'Varones'))
 
 
-##problema con Madrid y Andalucía
-codes <- as.data.frame(ficheropx, use.codes=TRUE)[,2]
-nms <- as.data.frame(ficheropx)[,2]
-data.frame(codes=codes, names=nms)
-##Madrid no está como provincia, sólo como Comunidad
-##...pero Andalucía sí está como Comunidad ¿¿??
+
 
