c7cf3c6f71573eac2c628f715f796dd83f3297bb
viciana <viciana@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-25 17:17:18 +0000

    fix as.px.array bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@90 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.px.array.R
+++ pkg/R/as.px.array.R
@@ -1,12 +1,12 @@
 #################################################################
-# 
+#
 # File:         as.px.array
 # Purpose:      Converts an array to a  px object
 #
 # Created:      20110801
 # Authors:      fvf, cjgb, opl
 #
-# Modifications: 
+# Modifications:
 #                 [130624] fvf: Check consistency between CODES and VALUES
 #
 #################################################################
@@ -26,82 +26,83 @@ as.px.array  <- function ( x, skeleton.px = NULL, list.keys = NULL, ...  )
 # skeleton.px <- oo
 # skeleton.px <- NULL
 # list.keys= list(MATRIX='xxxx', CONTENTS='cosas', MIA='prueba',
-#                 UNITS='personas', TITLE='titulos', DECIMAL='1') -> list.keys  
+#                 UNITS='personas', TITLE='titulos', DECIMAL='1') -> list.keys
 {
-    if ( ! is.array ( x ) ) 
-       stop('Error: object is not a "array"') 
- 
-  mkl1<- function (name,value)         {           
-              zz <- list( list(value=value) ) 
+    if ( ! is.array ( x ) )
+       stop('Error: object is not a "array"')
+
+  mkl1<- function (name,value)         {
+              zz <- list( list(value=value) )
               names( zz )  <- name
               return( zz )
            }
 
  if ( is.null(skeleton.px)) {
     opx <- c( mkl1( 'CHARSET','ANSI'),
-              mkl1( 'MATRIX','file000'),             
+              mkl1( 'MATRIX','file000'),
               mkl1( 'AXIS-VERSION','2000'),
               mkl1( 'SUBJECT-CODE','xx'),
               mkl1( 'SUBJECT-AREA','unknown'),
               mkl1( 'CONTENTS'    ,'unknown'),
-              mkl1( 'UNITS'       ,'unknown'),                          
+              mkl1( 'UNITS'       ,'unknown'),
               mkl1( 'TITLE'       ,'Title unknown'),
               mkl1( 'DECIMALS',0),
               mkl1( 'CREATION-DATE', format(Sys.time(), "%Y%m%d %H:%M:%S")),
               mkl1( 'LAST-UPDATED', format(Sys.time(), "%Y%m%d %H:%M:%S"))  )
     } else {  opx <- skeleton.px   }
 
-  
+
  ## Add key-value pair if list.keys<>NULL
- if (! is.null(list.keys)) {    
+ if (! is.null(list.keys)) {
     for (i in names(list.keys)) {
        # If there is key replaces it.
        # If it don't exist: adds new key
        if (is.null(opx[[i]])) {
                opx<-c(opx,mkl1(i, list.keys[[i]]))
-       } else opx[i] <- mkl1(i, list.keys[[i]])                  
+       } else opx[i] <- mkl1(i, list.keys[[i]])
     }
   }
 
  # delete STUB, HEADING, VALUES and DATA if there
  opx['STUB']   <- NULL ; opx['HEADING'] <- NULL
- opx['VALUES'] <- NULL ; opx['DATA']    <- NULL  
-  
+ opx['VALUES'] <- NULL ; opx['DATA']    <- NULL
+
  dd <- length( dim(x))
  if (dd > 1) {
      # First dim to 'header', the rest in reverse order
-     opx <- c(opx,     
+     opx <- c(opx,
              mkl1( 'STUB'   , rev(names(dimnames(x))[2:dd] )),
              mkl1( 'HEADING', names(dimnames(x))[1])   )
-             
+
      } else mkl1( 'STUB'   , rev(names(dimnames(x)) )) # Only one dim
 
-  
-  
+
+
  opx <- c( opx, list( 'VALUES' = dimnames(x) ) )
  opx <- c( opx, list( 'DATA'   = list( value = as.vector(x) ) ) )
-  
-  #  Delete skeleton.px$CODES: unused or inconsistent 
-  
+
+  #  Delete skeleton.px$CODES: unused or inconsistent
+
   if ( ! is.null(opx$CODES) ) {
 
     new.codes <- opx$CODES
     opx$CODES <- list()
-                
+
     new.codes <- new.codes [names(new.codes) %in% names(opx$VALUES)]
+
     new.codes <- new.codes[
-                     sapply(new.codes, length) == 
-                     sapply(opx$VALUES[names(opx$VALUES) %in% names(new.codes)],length)
+                     sapply(new.codes, length) ==
+                     sapply(opx$VALUES[names(new.codes)] ,length)
                      ]
-    
-    opx$CODES <- new.codes 
-    
+
+    opx$CODES <- new.codes
+
   }
-      
+
  class( opx ) <- "px"
-  
+
  return(opx)
-  
+
 }
 
 # ### example
@@ -110,11 +111,11 @@ as.px.array  <- function ( x, skeleton.px = NULL, list.keys = NULL, ...  )
 # as.px(aa)->px.aa1
 # as.px(aa,skeleton.px=oo )->px.aa2
 # as.array(px.aa1)->aa1
- 
+
 # ### export data checks
 # sum(as.array(aa)-as.array(px.aa1))
 # sum(as.array(aa)-as.array(px.aa2))
- 
+
 # ### Checks writing for missing data
 # oo  <- read.px(  system.file( "extdata", "example2.px", package = "pxR"))
 # aa  <-  as.array(oo)
@@ -126,7 +127,7 @@ as.px.array  <- function ( x, skeleton.px = NULL, list.keys = NULL, ...  )
 # write.px(as.px(aa,
 #              list.keys= list(MATRIX='xxx', CONTENTS='new data',
 #                              NEWKEY='an other key',
-#                              UNITS='people', TITLE='My Title') 
+#                              UNITS='people', TITLE='My Title')
 #                      ),file='tmpzxc03.px')
-# 
+#
 # file.remove('tmpzxc01.px','tmpzxc02.px','tmpzxc03.px')
