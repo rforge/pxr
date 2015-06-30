b6d0ce5339b8fd716db49973e3716787067aa726
cjgb <cjgb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-30 23:09:45 +0000

    Rewrite as.px.data.frame in terms of as.px.array
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pxr@106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testKEYS.R
+++ pkg/tests/testKEYS.R
@@ -1,12 +1,12 @@
 library(pxR)
 
-my.px  <- read.px( system.file( "extdata", "example.px", package = "pxR") )
+my.px  <- read.px(system.file("extdata", "example.px", package = "pxR"))
 
-my.df <- as.data.frame(my.px)
+my.df  <- as.data.frame(my.px)
 my.px2 <- as.px(my.df)         
 
-write.px ( my.px, filename = 'tmpzzx1.px' )  #  without KEYS
-write.px ( my.px, filename = 'tmpzzx2.px', keys = c( "municipios","edad"))  #  with KEYS
-write.px ( my.px2, filename = 'tmpzzx3.px' )  
+write.px(my.px, filename = 'tmpzzx1.px')  #  without KEYS
+write.px(my.px, filename = 'tmpzzx2.px', keys = c( "municipios","edad"))  #  with KEYS
+write.px(my.px2, filename = 'tmpzzx3.px')  
 
 file.remove('tmpzzx1.px','tmpzzx2.px', 'tmpzzx3.px')
